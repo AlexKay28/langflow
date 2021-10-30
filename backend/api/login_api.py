95252564bab310b75ebdd75984e114384554f50a
AlexKay28 <kaygorodow@mail.ru>
2021-10-30 15:51:48 +0300

    add is_anon key

--- backend/api/login_api.py
+++ backend/api/login_api.py
@@ -12,9 +12,15 @@ def login_api():
         req = request.get_json()
         username = req["username"]
         password = req["password"]
-        session_token_generated, user_existance = session.create_user(
-            username, password
-        )
+        is_anon = req["is_anon"]
+
+        if not is_anon:
+            session_token_generated, user_existance = session.create_user(
+                username, password
+            )
+        else:
+            pass  # TODO
+
         return jsonify(
             {"session_token": session_token_generated, "status": user_existance}
         )
